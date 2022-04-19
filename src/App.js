a4d0379cd29975d859681460b3eb0eecd81b9628
Red_devil <bharathram740@gmail.com>
2022-04-19 23:14:33 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Demo Form</h2>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Form from './Form';
+
+const Login = () => {
+    const [data, setData] = useState('')
+    
+
+    const handleChange = ({ target }) => {
+        const { name, value } = target
+        const newData = Object.assign({}, data, { [name]: value })
+        setData(newData)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(data)
+        document.cookie = `email=${data.email}`; 
+        document.cookie = `username=${data.username}`; 
+        document.cookie = `password=${data.password}`; 
+        console.log(document.cookie) 
+    }
+    return (
+        <div className='container'>
+            <Form
+                handleChange = {handleChange}
+                handleSubmit = {handleSubmit}
+                data = {data}
+            />
+        </div>
+    )
+}
+
+export default Login
